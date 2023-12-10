47b872a09485635ebdfbae041edaea97ee9ad895
Hua Xu <huaxu77@gmail.com>
2023-12-10 13:50:54 -0500

    Update gitignore

--- xumath/shortcuts.py
+++ xumath/shortcuts.py
@@ -5,23 +5,23 @@ GO_BACK = 1_000_001
 CHECK_SCORE = 1_000_003
 
 shortcuts = {
-    # ctrl + B
-    2: (GO_BACK, "Ctrl + B to go back to the precious menu."),
-    # ctrl + E
-    5: (EXIT_APP, "Ctrl + E to exit."),
-    # ctrl + X
-    24: (CHECK_SCORE, "Ctrl + X to check your score.")
+    "B": (GO_BACK, "to go back to the precious menu."),
+    "E": (EXIT_APP, "to exit."),
+    "X": (CHECK_SCORE, "to check your score."),
 }
 
 
 def getShortcuts():
-    return [v[1] for k, v in shortcuts.items()]
+    return ["Ctrl + %s %s" % (k, v[1]) for k, v in shortcuts.items()]
 
 
 def parseShortcuts(inp):
-    c = ord(inp[0]) if len(inp) == 1 else -1
-    c = shortcuts.get(c, [None])[0]
-    if c == EXIT_APP:
-        print("Exit the app!")
-        sys.exit()
-    return c
+    shortcut = None
+    if len(inp) == 1 and ord(inp[0]) <= 26:
+        c = chr(ord(inp[0]) + ord("A") - 1)
+        if c in shortcuts:
+            shortcut = shortcuts[c][0]
+            if shortcut == EXIT_APP:
+                print("Exit the app!")
+                sys.exit()
+    return shortcut
